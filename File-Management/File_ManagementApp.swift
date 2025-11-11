e21f77f0eb30d74a483c2f0ac31675fad85172b2
Mason Kimball <69182771+MasonKimball05@users.noreply.github.com>
2025-11-11 11:23:42 -0600

    Initial Commit

---
+++ File-Management/File_ManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  File_ManagementApp.swift
+//  File-Management
+//
+//  Created by Mason Kimball on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct File_ManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ File-ManagementTests/File_ManagementTests.swift
@@ -0,0 +1,17 @@
+//
+//  File_ManagementTests.swift
+//  File-ManagementTests
+//
+//  Created by Mason Kimball on 11/11/25.
+//
+
+import Testing
+@testable import File_Management
+
+struct File_ManagementTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
